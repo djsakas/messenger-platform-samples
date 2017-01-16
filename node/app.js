b5271ee56fe7210366758004dea6fa2c9431c522
djsakas <sakaswork@gmail.com>
2017-01-16 11:07:13 +0100

    Update app.js

--- node/app.js
+++ node/app.js
@@ -464,27 +464,6 @@ function qrQuantoMiSentoDerp(recipientId) {
   callSendAPI(messageData);
 }
 
-/*
- * Send an image using the Send API.
- *
- */
-function sendImageMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "image",
-        payload: {
-          url: SERVER_URL + "/assets/rift.png"
-        }
-      }
-    }
-  };
-
-  callSendAPI(messageData);
-}
 
 /*
  * Send a Gif using the Send API.
